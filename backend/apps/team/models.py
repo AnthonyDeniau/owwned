c4581c1f6ce9f1ba91c02375955c9452e039213f
JeyZu <jeremie.quatorze@gmail.com>
2019-07-18 15:01:25 +0200

    wrong syntax team/models.py

--- backend/apps/team/models.py
+++ backend/apps/team/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-Class Team(models.Model):
+class Team(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
