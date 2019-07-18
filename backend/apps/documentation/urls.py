fdf5832a7c6eb2ff121e2ca5e33a79d4f3371e88
Loïck Virot <virot.loick@gmail.com>
2019-07-18 14:54:59 +0200

    Initialize documentation

--- backend/apps/documentation/urls.py
+++ backend/apps/documentation/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    #example : path('', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'documentation.apps.DocumentationConfig'
+    'apps.documentation.apps.DocumentationConfig'
 ]
 
 MIDDLEWARE = [
