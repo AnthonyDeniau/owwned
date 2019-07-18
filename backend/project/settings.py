dbd498fc070db3f404ce8fa882478e0c2410eaf5
AnthonyDeniau <anthony.deniau@gmail.com>
2019-07-18 14:29:12 +0200

    add organization app

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APPS_DIR = os.path.realpath(os.path.join(BASE_DIR, 'apps'))
+sys.path.append(APPS_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
