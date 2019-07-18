a9ba7421cf8a4d0f924679f5298a963143a1b3e7
Mathilde <mathilde.aubignac@viacesi.fr>
2019-07-18 15:55:48 +0200

    Modification du type URL

--- backend/apps/documentation/models.py
+++ backend/apps/documentation/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Documentation(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    url = models.CharField(max_length=255)
-    docfile = models.FileField(max_length=255)
+    # asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=False)
+    url = models.URLField(max_length=255, default=None, null=True)
+    docfile = models.FileField(max_length=255, default=None, null=True)
     
     def str(self):
         return self.name
