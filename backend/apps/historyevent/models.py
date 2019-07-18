2e778d0eaea6278cac4cfda929e434786a31c00b
dsanson <dylan.sanson@viacesi.fr>
2019-07-18 16:21:28 +0200

    add model

--- backend/apps/historyevent/models.py
+++ backend/apps/historyevent/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from asset.models import Asset
 
-# Create your models here.
+class HistoryEvent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+    CHOICES = [
+        ('Lend', 'Lend'),
+        ('Maintenance', 'Maintenance'),
+        ('Other', 'Other'),
+    ]
+    typeEvent = models.CharField(
+        max_length=30,
+        choices=CHOICES,
+        default='Other',
+    )
+    description = models.TextField(max_length=500)
+
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles', 'organization', 'supplier'
+    'django.contrib.messages', 'django.contrib.staticfiles', 'organization', 'supplier', 'historyevent', 'asset', 'team',
 ]
 
 MIDDLEWARE = [
