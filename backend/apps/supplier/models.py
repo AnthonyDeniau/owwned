3bdc2df1a31aec95591444210b4fcc14358591fb
Travel toon <toon210792@gmail.com>
2019-07-18 15:00:46 +0200

    create supplier model

--- backend/apps/supplier/models.py
+++ backend/apps/supplier/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Name(models.Model):
+    name_test = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name_test
+
+
+class WebSite(models.Model):
+    webSiteUrl_text = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.webSiteUrl_text
+
+class Login(models.Model):
+    login_text = models.CharField
+    def __str__(self):
+        return self.login_text
+
+
+class Password(models.Model):
+    password_text = models.CharField(max_length=20)
+    def __str__(self):
+        return self.password_text
