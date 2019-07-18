e0554b9594e858f3915f98b6d4c2e11756e51bee
Travel toon <toon210792@gmail.com>
2019-07-18 15:09:19 +0200

    add permission in admin.py to edit them online

--- backend/apps/supplier/admin.py
+++ backend/apps/supplier/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Name, WebSite, Login, Password
 # Register your models here.
+
+admin.site.register(Name)
+admin.site.register(WebSite)
+admin.site.register(Login)
+admin.site.register(Password)
