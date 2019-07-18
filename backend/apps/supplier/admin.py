b010abdd9ac444537b5c33a5b6b039cde9bb7c77
Travel toon <toon210792@gmail.com>
2019-07-18 16:35:03 +0200

    migrate model

--- backend/apps/supplier/admin.py
+++ backend/apps/supplier/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 
-from .models import Name, WebSite, Login, Password
+from .models import Supplier
 # Register your models here.
 
-admin.site.register(Name)
-admin.site.register(WebSite)
-admin.site.register(Login)
-admin.site.register(Password
+admin.site.register(Supplier)
