19467df464e3a42806bfeeae89a2a0d6932bbfe4
JeyZu <jeremie.quatorze@gmail.com>
2019-07-18 14:59:18 +0200

    add team/models.py

--- backend/apps/team/models.py
+++ backend/apps/team/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+Class Team(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
