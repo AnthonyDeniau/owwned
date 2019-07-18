c3f3a6b75042d23384e65b9ef9c6b0d756143d9d
AnthonyDeniau <anthony.deniau@gmail.com>
2019-07-18 16:30:47 +0200

    fix decimal places

--- backend/apps/asset/models.py
+++ backend/apps/asset/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from team.models import Team
-from supplier.models import Supplier
+
+# from supplier.models import Supplier
+
 
 # Create your models here.
 class Asset(models.Model):
@@ -8,6 +10,5 @@ class Asset(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     picture = models.ImageField()
-    cost = models.DecimalField(decimal_place=2, max_digits=19)
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-
+    cost = models.DecimalField(decimal_places=2, max_digits=19)
+    # supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
