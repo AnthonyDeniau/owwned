d98b7d27608b4155156b768e6a867427148461bd
AnthonyDeniau <anthony.deniau@gmail.com>
2019-07-25 17:51:01 +0200

    add mutation in general schema

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -7,4 +7,8 @@ class Query(organization.schema.Query, team.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(team.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
