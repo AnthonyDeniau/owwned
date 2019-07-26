fff475c52d49d46da2818f546ec3e79a3007cc81
AnthonyDeniau <anthony.deniau@gmail.com>
2019-07-26 15:12:34 +0200

    mes images...

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -32,21 +32,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'organization',
-    'supplier',
-    'historyevent',
-    'asset',
-    'team',
-    'profiles',
-    'graphene_django',
-    'documentation'
+    'django.contrib.admin', 'django.contrib.auth',
+    'django.contrib.contenttypes', 'django.contrib.sessions',
+    'django.contrib.messages', 'django.contrib.staticfiles', 'corsheaders',
+    'organization', 'supplier', 'historyevent', 'asset', 'team', 'profiles',
+    'graphene_django', 'documentation'
 ]
 
 GRAPHENE = {
@@ -135,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
