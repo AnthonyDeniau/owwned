477ab350ddb3b9146e5196de2781503f3f1dbbe6
Kaelsier <remi.martin@viacesi.fr>
2019-07-26 15:16:18 +0200

    localisation creation

---
+++ backend/apps/localisation/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Batiment(models.Model):
+    lat = models.DecimalField(decimal_places=2, max_length=19)
+    long = models.DecimalField(decimal_places=2, max_length=19)
+    name = models.CharField(max_length=255)
+
+    def str(self):
+        return self.name
+
+
+class Floor(models.Model):
+    batiment = models.ForeignKey(Batiment, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def str(self):
+        return self.name
+
+
+class Room(models.Model):
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def str(self):
+        return self.name
---
+++ backend/apps/localisation/schema.py
@@ -0,0 +1,42 @@
+from graphene_django import DjangoObjectType
+import graphene
+from .models import Batiment
+from .models import Floor
+from .models import Room
+
+
+class DocumentationType(DjangoObjectType):
+    class Meta:
+        model = Batiment
+
+class CreateLocalisation(graphene.Mutation):
+    id = graphene.Int()
+    name = graphene.String()
+    description = graphene.String()
+    url = graphene.String()
+    docfile = graphene.String()
+
+    class Arguments:        
+        asset = graphene.ID()
+        name = graphene.String(required=True)
+        description = graphene.String()
+        url = graphene.String()
+        docfile = graphene.String()
+
+    def mutate(self, info, asset, name, description, url, docfile):
+        documentation = Documentation(asset_id=asset, name=name, description=description, url=url, docfile=docfile)
+        documentation.save()
+        return CreateDocumentation(id=documentation.id,
+                                    asset=documentation.asset,
+                                    name=documentation.name,
+                                    description=documentation.description,
+                                    url=documentation.url,
+                                    docfile=documentation.docfile)
+
+
+
+class Mutation(graphene.ObjectType):
+    create_localisation = CreateLocalisation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
