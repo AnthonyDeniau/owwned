f390aa756fec2684dfc959932505c8db18385a49
Kaelsier <remi.martin@viacesi.fr>
2019-07-26 15:59:14 +0200

    Creation mutation batiment
    Creatin query batiment

--- backend/apps/localisation/models.py
+++ backend/apps/localisation/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Batiment(models.Model):
-    lat = models.DecimalField(decimal_places=2, max_length=19)
-    long = models.DecimalField(decimal_places=2, max_length=19)
+    lat = models.DecimalField(decimal_places=2, max_digits=19)
+    long = models.DecimalField(decimal_places=2, max_digits=19)
     name = models.CharField(max_length=255)
 
-    def str(self):
+    def __str__(self):
         return self.name
 
 
@@ -14,7 +14,7 @@ class Floor(models.Model):
     batiment = models.ForeignKey(Batiment, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
 
-    def str(self):
+    def __str__(self):
         return self.name
 
 
@@ -22,5 +22,5 @@ class Room(models.Model):
     floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
 
-    def str(self):
+    def __str__(self):
         return self.name
--- backend/apps/localisation/schema.py
+++ backend/apps/localisation/schema.py
@@ -5,38 +5,128 @@ from .models import Floor
 from .models import Room
 
 
-class DocumentationType(DjangoObjectType):
+class BatimentType(DjangoObjectType):
     class Meta:
         model = Batiment
 
-class CreateLocalisation(graphene.Mutation):
+
+class FloorType(DjangoObjectType):
+    class Meta:
+        model = Floor
+
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
+
+
+class CreateBatiment(graphene.Mutation):
     id = graphene.Int()
+    lat = graphene.Decimal()
+    long = graphene.Decimal()
     name = graphene.String()
-    description = graphene.String()
-    url = graphene.String()
-    docfile = graphene.String()
 
-    class Arguments:        
-        asset = graphene.ID()
+    class Arguments:
+        long = graphene.Decimal(required=True)
+        lat = graphene.Decimal(required=True)
         name = graphene.String(required=True)
-        description = graphene.String()
-        url = graphene.String()
-        docfile = graphene.String()
 
-    def mutate(self, info, asset, name, description, url, docfile):
-        documentation = Documentation(asset_id=asset, name=name, description=description, url=url, docfile=docfile)
-        documentation.save()
-        return CreateDocumentation(id=documentation.id,
-                                    asset=documentation.asset,
-                                    name=documentation.name,
-                                    description=documentation.description,
-                                    url=documentation.url,
-                                    docfile=documentation.docfile)
+    def mutate(self, info, long, lat, name):
+        batiment = Batiment(long=long, lat=lat, name=name)
+        batiment.save()
+        return CreateBatiment(
+            id=batiment.id,
+            long=batiment.long,
+            lat=batiment.lat,
+            name=batiment.name)
 
+
+class QueryBatiment(graphene.ObjectType):
+    batiment = graphene.Field(BatimentType, id=graphene.Int(), name=graphene.String())
+    batiments = graphene.List(BatimentType)
+
+    def resolve_batiment(self, context, id=None, name=None):
+        if id is not None:
+            return Batiment.objects.get(pk=id)
+
+        if name is not None:
+            return Batiment.objects.get(name=name)
+
+        return None
+
+    def resolve_batiments(self, context):
+        return Batiment.objects.all()
+
+
+class UpdateBatiment(graphene.Mutation):
+    id = graphene.Int()
+    lat = graphene.Decimal()
+    long = graphene.Decimal()
+    name = graphene.String()
+
+    class Arguments:
+        id = graphene.Int(required=True)
+        long = graphene.Decimal(required=True)
+        lat = graphene.Decimal(required=True)
+        name = graphene.String(required=True)
+
+    def mutate(self, info, id, long=None, lat=None, name=None):
+        batiment = Batiment.objects.get(pk=id)
+        if long is not None:
+            batiment.asset_id = long
+        if lat is not None:
+            batiment.name = lat
+        if name is not None:
+            batiment.description = name
+        batiment.save()
+        return UpdateBatiment(id=batiment.id,
+                              long=batiment.long,
+                              lat=batiment.lat,
+                              name=batiment.name)
+
+
+class DeleteBatiment(graphene.Mutation):
+    is_delete = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID()
+
+    def mutate(self, info, id):
+        batiment = Batiment.objects.get(pk=id)
+        batiment.delete()
+        return DeleteBatiment(is_delete=True)
+
+
+class DeleteFloor(graphene.Mutation):
+    is_delete = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID()
+
+    def mutate(self, info, id):
+        floor = Floor.objects.get(pk=id)
+        floor.delete()
+        return DeleteFloor(is_delete=True)
+
+
+class DeleteRoom(graphene.Mutation):
+    is_delete = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID()
+
+    def mutate(self, info, id):
+        room = Room.objects.get(pk=id)
+        room.delete()
+        return DeleteRoom(is_delete=True)
 
 
 class Mutation(graphene.ObjectType):
-    create_localisation = CreateLocalisation.Field()
+    create_batiment = CreateBatiment.Field()
+    update_batiment = UpdateBatiment.Field()
+    delete_batiment = DeleteBatiment.Field()
+    delete_floor = DeleteFloor.Field()
+    delete_room = DeleteRoom.Field()
 
 
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema(query=QueryBatiment, mutation=Mutation)
