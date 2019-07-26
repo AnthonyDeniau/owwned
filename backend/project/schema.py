24c934385b5c2258ed7621a752aea23a94adbf94
Danid <danid.said@gmail.com>
2019-07-26 10:49:44 +0200

    History mutation

--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,14 +1,13 @@
 import graphene
 import organization.schema
 import team.schema
-import historyevent.schema
 
 
-class Query(organization.schema.Query, team.schema.Query, historyevent.schema.Query, graphene.ObjectType):
+class Query(organization.schema.Query, team.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(team.schema.Mutation, historyevent.schema.Mutation, graphene.ObjectType):
+class Mutation(team.schema.Mutation, graphene.ObjectType):
     pass
 
 
