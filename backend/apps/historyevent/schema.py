a91cd4dee5f17f89fdcdfff6182cb207fa740e61
Danid <danid.said@gmail.com>
2019-07-26 11:33:58 +0200

    history

--- backend/apps/historyevent/schema.py
+++ backend/apps/historyevent/schema.py
@@ -1,7 +1,7 @@
 from graphene_django import DjangoObjectType
 import graphene
 from .models import HistoryEvent
-#from asset.schema import AssetType
+from asset.schema import AssetType
 
 class HistoryEventType(DjangoObjectType):
     class Meta:
@@ -10,6 +10,7 @@ class HistoryEventType(DjangoObjectType):
 class CreateHistoryEvent(graphene.Mutation):
     id = graphene.Int()
     user = graphene.ID()
+    asset = graphene.ID()
     startDate = graphene.types.datetime.DateTime()
     endDate = graphene.types.datetime.DateTime()
     typeEvent = graphene.String()
@@ -17,13 +18,13 @@ class CreateHistoryEvent(graphene.Mutation):
 
     class Arguments:
         user = graphene.ID(required=True)
-        #asset = graphene.ID()
+        asset = graphene.ID()
         startDate = graphene.types.datetime.DateTime(required=True)
         endDate = graphene.types.datetime.DateTime()
         typeEvent = graphene.String(required=True)
         description = graphene.String()
 
-    def mutate(self, info, user, startDate, endDate, typeEvent, description):
+    def mutate(self, info, user, asset, startDate, endDate, typeEvent, description):
         historyEvent = HistoryEvent(user_id=user,asset_id=1, startDate=startDate, endDate=endDate, typeEvent=typeEvent, description=description)
         historyEvent.save()
         return CreateHistoryEvent(id=historyEvent.id,
