ef56a7a12b86e8a23784497bf534b14a40f56c9e
Morgan Nicol <nicol.morgan@outlook.fr>
2019-07-26 16:18:20 +0200

    update & create & delete building

--- backend/apps/building/schema.py
+++ backend/apps/building/schema.py
@@ -26,6 +26,42 @@ class CreateBuilding(graphene.Mutation):
                               lat=building.long)
 
 
+class DeleteBuilding(graphene.Mutation):
+    status = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    def mutate(self, info, id):
+        building = Building.objects.get(pk=id)
+        building.delete()
+        return DeleteBuilding(
+            status=True
+        )
+
+
+class UpdateBuilding(graphene.Mutation):
+    name = graphene.String()
+    long = graphene.Decimal()
+    lat = graphene.Decimal()
+
+    class Arguments:
+        name = graphene.String()
+        long = graphene.Decimal()
+        lat = graphene.Decimal()
+
+    def mutate(self, info, name, long, lat):
+        building = Building.objects.get(pk=id)
+        building.name = name
+        building.lat = lat
+        building.long = long
+
+        building.save()
+        return UpdateBuilding(name=building.name,
+                              long=building.lat,
+                              lat=building.long)
+
+
 class Query(graphene.ObjectType):
     building = graphene.Field(CreateBuilding, id=graphene.Int())
     buildings = graphene.List(CreateBuilding)
@@ -40,6 +76,8 @@ class Query(graphene.ObjectType):
 
 class Mutation(graphene.ObjectType):
     create_building = CreateBuilding.Field()
+    delete_building = DeleteBuilding.Field()
+    update_building = UpdateBuilding.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
