4e74ec3afb286f02e0ec5a67ae205815b8cd1836
adamnasri <adam.nasri31270@gmail.com>
2019-07-26 16:29:40 +0200

    Added a mutation and a query

--- backend/apps/Location/schema.py
+++ backend/apps/Location/schema.py
@@ -1,18 +1,41 @@
 from graphene_django import DjangoObjectType
 import graphene
-from Location import Batiment
+from .models import Batiment
+from .models import Floor
+from .models import Room
+
+class BatimentType(DjangoObjectType):
+    class Meta:
+        model = Batiment
+
+class FloorType(DjangoObjectType):
+    class Meta:
+        model = Floor
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
+
+class Query(graphene.ObjectType):
+    batiment = graphene.Field(BatimentType, id=graphene.Int())
+    batiments = graphene.List(BatimentType)
 
 class CreateBatiment(graphene.Mutation):
-    Lat = graphene.DecimalField()
-    Long = graphene.DecimalField()
+    Lat = graphene.Float()
+    Long = graphene.Float()
     Name = graphene.String()
 
     class Arguments:
-        Lat = graphene.types.DecimalField()
-        Long = graphene.types.DecimalField()
+        Lat = graphene.Float()
+        Long = graphene.Float()
         Name = graphene.String()
     
     def mutate(self, info):
-        batiment = Location.Batiment(Lat=Lat, Long=Long, Name=Name)
+        batiment = Batiment(Lat=Lat, Long=Long, Name=Name)
         batiment.save()
         return CreateBatiment(Lat=batiment.Lat, Long=batiment.Long, Name=batiment.name)
+
+class Mutation(graphene.ObjectType):
+    create_Batiment = CreateBatiment.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
