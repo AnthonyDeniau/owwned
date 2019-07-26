d7de504389ecc3ea72cfb44503b45bd6550fc493
Benjamin Corre <benjii3176@gmail.com>
2019-07-26 13:52:05 +0200

    added update method for profile

--- backend/apps/profiles/schema.py
+++ backend/apps/profiles/schema.py
@@ -38,6 +38,28 @@ class CreateProfiles(graphene.Mutation):
                               team=profile.team)
 
 
+class UpdateProfiles(graphene.Mutation):
+    id = graphene.Int()
+    user = graphene.ID()
+    team = graphene.ID()
+
+    class Arguments:
+        id = graphene.Int()
+        user = graphene.ID()
+        team = graphene.ID()
+
+    def mutate(self, info, id, user=None, team=None):
+        profile = Profiles.objects.get(pk=id)
+        if (user is not None):
+            profile.user = user
+        if (team is not None):
+            profile.team = team
+        profile.save()
+        return UpdateProfiles(id=profile.id,
+                              user=profile.user,
+                              team=profile.team)
+
+
 class DeleteProfiles(graphene.Mutation):
     is_delete = graphene.Boolean()
 
@@ -52,6 +74,7 @@ class DeleteProfiles(graphene.Mutation):
 
 class Mutation(graphene.ObjectType):
     create_profile = CreateProfiles.Field()
+    update_profile = UpdateProfiles.Field()
     delete_profile = DeleteProfiles.Field()
 
 
