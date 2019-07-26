c6f5c56e840d8ff9c5a58360909a7a7f58a5a003
Kaelsier <remi.martin@viacesi.fr>
2019-07-26 11:10:04 +0200

    Modification shema documentation

--- backend/apps/documentation/schema.py
+++ backend/apps/documentation/schema.py
@@ -1,3 +1,31 @@
 from graphene_django import DjangoObjectType
 import graphene
 from .models import Documentation
+
+
+class DocumentationType(DjangoObjectType):
+    class Meta:
+        model = Documentation
+
+
+class Query(graphene.ObjectType):
+    is_query = graphene.Boolean()
+
+
+class DeleteDocumentation(graphene.Mutation):
+    is_delete = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID()
+
+    def mutate(self, info, id):
+        documentation = Documentation.objects.get(pk=id)
+        documentation.delete()
+        return DeleteDocumentation(is_delete=True)
+
+
+class Mutation(graphene.ObjectType):
+    deleteDocumentation = DeleteDocumentation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- backend/project/schema.py
+++ backend/project/schema.py
@@ -1,13 +1,14 @@
 import graphene
 import organization.schema
 import team.schema
+import documentation.schema
 
 
-class Query(organization.schema.Query, team.schema.Query, graphene.ObjectType):
+class Query(organization.schema.Query, team.schema.Query, documentation.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(team.schema.Mutation, graphene.ObjectType):
+class Mutation(team.schema.Mutation,documentation.schema.Mutation, graphene.ObjectType):
     pass
 
 
