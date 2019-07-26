25015141a3b5c253a0c0dd42f14d08e48f048a2a
Morgan Nicol <nicol.morgan@outlook.fr>
2019-07-26 15:26:45 +0200

    model

--- backend/apps/building/models.py
+++ backend/apps/building/models.py
@@ -2,10 +2,26 @@ from django.db import models
 
 
 class Building(models.Model):
-    name = models.DecimalField(decimal_places=2, max_digits=19)
+    name = models.CharField(max_length=100)
     lat = models.DecimalField(decimal_places=2, max_digits=19)
     long = models.DecimalField(decimal_places=2, max_digits=19)
 
     def __str__(self):
         return self.name
 
+
+class Floor(models.Model):
+    name = models.CharField(max_length=100)
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+
+
+def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
