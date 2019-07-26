63b913d23a567d6c684bebe78620a7dbd9c33956
Mathilde <mathilde.aubignac@viacesi.fr>
2019-07-26 10:30:44 +0200

    Ajout Select DOCUMENTATION

--- backend/apps/documentation/schema.py
+++ backend/apps/documentation/schema.py
@@ -1,3 +1,23 @@
 from graphene_django import DjangoObjectType
 import graphene
 from .models import Documentation
+
+class DocumentationType(DjangoObjectType):
+    class Meta:
+        model = Documentation
+
+class Query(graphene.ObjectType):
+    documentation = graphene.Field(DocumentationType, id=graphene.Int(), name=graphene.String())
+    documentations = graphene.List(DocumentationType)
+
+    def resolve_documentation(self, context, id=None, name=None):
+        if id is not None:
+            return Documentation.objects.get(pk=id)
+
+        if name is not None:
+            return Documentation.objects.get(name=name)
+
+        return None
+
+    def resolve_documentations(self, context):
+        return Documentation.objects.all()
