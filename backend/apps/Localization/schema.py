f9ddec3046006bccbfb712f1985a4cf8e439e7e4
DESKTOP-5CPL9JS\Viko <victoria.gardey@live.fr>
2019-07-26 16:04:50 +0200

    correction, attention aux majuscules

--- backend/apps/Localization/schema.py
+++ backend/apps/Localization/schema.py
@@ -18,45 +18,45 @@ class RoomType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    building = graphene.Field(BuildingType, id=graphene.Int(), name=graphene.String())
+    building = graphene.Field(BuildingType, id=graphene.Int(), Name=graphene.String())
     buildings = graphene.List(BuildingType)
 
-    floor = graphene.Field(FloorType, id=graphene.Int(), name=graphene.String())
+    floor = graphene.Field(FloorType, id=graphene.Int(), Name=graphene.String())
     floors = graphene.List(FloorType) 
 
-    room = graphene.Field(RoomType, id=graphene.Int(), name=graphene.String())
+    room = graphene.Field(RoomType, id=graphene.Int(), Name=graphene.String())
     rooms = graphene.List(RoomType)
 
-    def resolve_building(self, context, id=None, name=None):
+    def resolve_building(self, context, id=None, Name=None):
         if id is not None:
             return Building.objects.get(pk=id)
 
-        if name is not None:
-            return Building.objects.get(name=name)
+        if Name is not None:
+            return Building.objects.get(Name=Name)
 
         return None
 
     def resolve_buildings(self, context):
         return Building.objects.all()
     
-    def resolve_floor(self, context, id=None, name=None):
+    def resolve_floor(self, context, id=None, Name=None):
         if id is not None:
             return Floor.objects.get(pk=id)
 
-        if name is not None:
-            return Floor.objects.get(name=name)
+        if Name is not None:
+            return Floor.objects.get(Name=Name)
 
         return None
 
     def resolve_floors(self, context):
         return Floor.objects.all()
 
-    def resolve_room(self, context, id=None, name=None):
+    def resolve_room(self, context, id=None, Name=None):
         if id is not None:
             return Room.objects.get(pk=id)
 
-        if name is not None:
-            return Room.objects.get(name=name)
+        if Name is not None:
+            return Room.objects.get(Name=Name)
 
         return None
 
@@ -79,9 +79,9 @@ class CreateBuilding(graphene.Mutation):
         Building = Building(Lat=Lat, Long=Long, Name=Name)
         Building.save()
         return CreateBuilding(id=Building.id,
-                            Lat=Building.lat,
-                            Long=Building.long,
-                            name=Building.name)
+                            Lat=Building.Lat,
+                            Long=Building.Long,
+                            name=Building.Name)
 
 class Mutation(graphene.ObjectType):
     createbuilding = CreateBuilding.Field()
