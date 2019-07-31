a07e34c40f8aa8eec5ba0cc27112a2ccb1fb2545
AnthonyDeniau <anthony.deniau@gmail.com>
2019-07-31 08:01:11 +0200

    corrections

--- backend/apps/location/schema.py
+++ backend/apps/location/schema.py
@@ -5,9 +5,21 @@ from asset.models import Asset
 
 from asset.schema import AssetType
 
+
 class BatimentType(DjangoObjectType):
     class Meta:
         model = Batiment
+
+
+class FloorType(DjangoObjectType):
+    class Meta:
+        model = Floor
+
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
+
 
 class Query(graphene.ObjectType):
     batiment = graphene.Field(BatimentType, id=graphene.Int())
@@ -20,13 +32,13 @@ class Query(graphene.ObjectType):
 
         return None
 
-        
     def resolve_batiments(self, context):
         return Batiment.objects.all()
 
     def resolve_assetsBatiment(self, context, batimentId):
-        return Asset.objects.filter(Floor_id__in =  Floor.objects.filter(Room_id__in = ))
-        return Asset.objects.filter(reduce(operator.and_, (Q(first_name__contains=x) for x in ['x', 'y', 'z']))
+        # return Asset.objects.filter(Floor_id__in =  Floor.objects.filter(Room_id__in = ))
+        return Asset.objects.filter(room__floor__batiment_id=batimentId)
+
 
 class CreateBatiment(graphene.Mutation):
     id = graphene.Int()
@@ -40,12 +52,13 @@ class CreateBatiment(graphene.Mutation):
         name = graphene.String(required=True)
 
     def mutate(self, info, lat, longBat, name):
-        batiment = Batiment(lat=lat,longBat=longBat, name=name)
+        batiment = Batiment(lat=lat, longBat=longBat, name=name)
         batiment.save()
         return CreateBatiment(id=batiment.id,
-                                    lat=batiment.lat,
-                                    longBat=batiment.longBat,
-                                    name=batiment.name)
+                              lat=batiment.lat,
+                              longBat=batiment.longBat,
+                              name=batiment.name)
+
 
 class UpdateBatiment(graphene.Mutation):
     id = graphene.Int()
@@ -59,19 +72,20 @@ class UpdateBatiment(graphene.Mutation):
         longBat = graphene.Int()
         name = graphene.String()
 
-    def mutate(self, info, id,lat=None , longBat=None, name=None):
+    def mutate(self, info, id, lat=None, longBat=None, name=None):
         batiment = Batiment.objects.get(pk=id)
         if lat is not None:
             batiment.lat = lat
         if longBat is not None:
-            batiment.longBat = longBat 
+            batiment.longBat = longBat
         if name is not None:
             batiment.name = name
 
         batiment.save()
         return UpdateBatiment(lat=batiment.lat,
-                                    longBat=batiment.longBat,
-                                    name=batiment.name)
+                              longBat=batiment.longBat,
+                              name=batiment.name)
+
     #test
 
 
@@ -87,9 +101,10 @@ class DeleteBatiment(graphene.Mutation):
         return DeleteBatiment(batimentBool=True)
 
 
-
 class Mutation(graphene.ObjectType):
     create_batiment = CreateBatiment.Field()
     delete_batiment = DeleteBatiment.Field()
     update_batiment = UpdateBatiment.Field()
+
+
 schema = graphene.Schema(query=Query, mutation=Mutation)
