bf354d85b5e3f9a4b518539029a400d61e82c883
AnthonyDeniau <anthony.deniau@gmail.com>
2019-07-31 07:46:46 +0200

    corrections

--- backend/apps/location/schema.py
+++ backend/apps/location/schema.py
@@ -3,14 +3,25 @@ import graphene
 from .models import Batiment, Floor, Room
 
 
-
 class BatimentType(DjangoObjectType):
     class Meta:
         model = Batiment
 
 
+class FloorType(DjangoObjectType):
+    class Meta:
+        model = Floor
+
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
+
+
 class Query(graphene.ObjectType):
-    batiment = graphene.Field(BatimentType, id=graphene.Int(), name=graphene.String())
+    batiment = graphene.Field(BatimentType,
+                              id=graphene.Int(),
+                              name=graphene.String())
     batiments = graphene.List(BatimentType)
 
     def resolve_batiment(self, context, id=None, name=None):
@@ -32,24 +43,22 @@ class CreateBatiment(graphene.Mutation):
     longitude = graphene.Float()
     name = graphene.String()
 
-    class Arguments:        
+    class Arguments:
         latitude = graphene.Float()
         longitude = graphene.Float()
         name = graphene.String(required=True)
-        
 
     def mutate(self, info, latitude, longitude, name):
-        batiment = Batiment(latitude=latitude,longitude=longitude,name=name)
+        batiment = Batiment(latitude=latitude, longitude=longitude, name=name)
         batiment.save()
-        return CreateBatiment(latitude = batiment.latitude,
-                                   longitude = batiment.longitude,
-                                   name = batiment.name)
+        return CreateBatiment(id=batiment.pk,
+                              latitude=batiment.latitude,
+                              longitude=batiment.longitude,
+                              name=batiment.name)
 
 
 class Mutation(graphene.ObjectType):
     create_batiment = CreateBatiment.Field()
-
-
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
